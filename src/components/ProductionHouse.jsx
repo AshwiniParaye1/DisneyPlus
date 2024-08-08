8d2e48d2ba0c07b09df68a6f3030318be9ced81f
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-08 19:18:08 +0530

    added bg videos

--- src/components/ProductionHouse.jsx
+++ src/components/ProductionHouse.jsx
@@ -46,7 +46,15 @@ function ProductionHouse() {
           key={item.id}
           className="border-[2px] border-gray-600 rounded-lg hover:scale-110 transition-all duration-300 cursor-pointer ease-in-out"
         >
-          <img src={item.image} className="w-full" />
+          <video
+            src={item.video}
+            autoPlay
+            loop
+            playsInline
+            muted
+            className="absolute top-0 rounded-md z-0 opacity-0 hover:opacity-50"
+          />
+          <img src={item.image} className="w-full z-[1] opacity-100" />
         </div>
       ))}
     </div>
