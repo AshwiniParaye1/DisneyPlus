c498bc41173925ab4902185539195ba2608c68a4
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-09 11:49:11 +0530

    fixed api error

--- src/services/GlobalApi.jsx
+++ src/services/GlobalApi.jsx
@@ -4,7 +4,7 @@ const movieBaseUrl = "https://api.themoviedb.org/3";
 const api_key = "f6de5422c2c42314a865d48be52b38aa";
 
 const movieByGenreBaseUrl =
-  "https://api.themoviedb.org/3/discover/movie?api_key=${api_key}";
+  "https://api.themoviedb.org/3/discover/movie?api_key=f6de5422c2c42314a865d48be52b38aa";
 
 // https://api.themoviedb.org/3/trending/all/day?api_key=f6de5422c2c42314a865d48be52b38aa
 
@@ -12,8 +12,7 @@ const getTrendingMovie = axios.get(
   movieBaseUrl + "/trending/all/day?api_key=" + api_key
 );
 
-const getMovieByGenreId = (id) => {
+const getMovieByGenreId = (id) =>
   axios.get(movieByGenreBaseUrl + "&with_genres=" + id);
-};
 
 export default { getTrendingMovie, getMovieByGenreId };
