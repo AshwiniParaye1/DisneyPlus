1da4e291cb03b964191bc1f7bb3264248f508c9e
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-09 16:51:30 +0530

    modified footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,96 +1,166 @@
 import React from "react";
+import { BsTwitterX } from "react-icons/bs";
+import { IoCheckmark } from "react-icons/io5";
+import { TbBrandFacebook } from "react-icons/tb";
+import GooglePlayImg from "../assets/images/footerImages/google-playstore.webp";
+import AppStoreImg from "../assets/images/footerImages/appstore.webp";
 
 function Footer() {
   return (
-    <div className="bg-black text-white p-5 px-8 md:px-16 mt-[96px]">
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-8">
+    <div className="text-white p-5 px-8 md:px-16 mt-[96px]">
+      <div
+        className="flex
+        justify-between mb-12"
+      >
         <div>
-          <h4 className="text-xs font-medium mb-2">Company</h4>
-          <ul className="space-y-1">
+          <h4 className="text-sm font-medium mb-3">Company</h4>
+          <ul className="space-y-1 text-xs">
             <li>
-              <a href="#" className="text-sm ">
+              <a
+                href="https://www.hotstar.com/about-us/in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-gray-400"
+              >
                 About Us
               </a>
             </li>
             <li>
-              <a href="#" className="text-sm ">
+              <a
+                href="https://careers.hotstar.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-gray-400"
+              >
                 Careers
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-sm ">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-sm ">
-                Terms of Use
               </a>
             </li>
           </ul>
         </div>
         <div>
-          <h4 className="text-xs font-medium mb-2">View Website in</h4>
-          <ul className="space-y-1">
-            <li>
-              <a href="#" className="text-sm ">
+          <h4 className="text-sm font-medium mb-3">View Website in</h4>
+          <ul className="space-y-1 text-xs">
+            <li className="flex items-center">
+              <IoCheckmark className="mr-2" size={20} />
+              <a
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-gray-400"
+              >
                 English
               </a>
             </li>
           </ul>
         </div>
         <div>
-          <h4 className="text-xs font-medium mb-2">Need Help?</h4>
-          <ul className="space-y-1">
+          <h4 className="text-sm font-medium mb-3">Need Help?</h4>
+          <ul className="space-y-1 text-xs">
             <li>
-              <a href="#" className="text-sm ">
-                FAQ
+              <a
+                href="https://help.hotstar.com/in/en/support/home"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-gray-400"
+              >
+                Visit Help Center
               </a>
             </li>
             <li>
-              <a href="#" className="text-sm ">
-                Contact Us
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-sm ">
-                Support
+              <a
+                href="https://help.hotstar.com/in/en/support/tickets/feedback"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-gray-400"
+              >
+                Share Feedback
               </a>
             </li>
           </ul>
         </div>
         <div>
-          <h4 className="text-xs font-medium mb-2">Connect with Us</h4>
-          <ul className="space-y-1">
-            <li>
-              <a href="#" className="text-sm ">
-                Facebook
+          <h4 className="text-sm font-medium mb-3">Connect with Us</h4>
+          <ul className="space-y-1 text-xs flex">
+            <li className="mr-6 flex items-center">
+              {" "}
+              <a
+                href="https://www.facebook.com/DisneyPlusHotstar"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:text-gray-400"
+              >
+                <TbBrandFacebook size={25} />
               </a>
             </li>
-            <li>
-              <a href="#" className="text-sm ">
-                Twitter
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-sm ">
-                Instagram
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-sm ">
-                LinkedIn
+            <li className="flex items-center">
+              <a
+                href="https://x.com/DisneyPlusHS"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:text-gray-400"
+              >
+                <BsTwitterX size={17} />
               </a>
             </li>
           </ul>
         </div>
       </div>
-      <div className="mt-8  text-xs text-gray-400">
-        <p className="mb-2">&copy; 2024 STAR. All rights reserved.</p>
-        <p>
-          <span className="mr-2">Terms of Use</span>{" "}
-          <span className="mr-2">Privacy Policy</span> <span>FAQ</span>
-        </p>
+      <div className="footerBottom flex justify-between">
+        <div className=" text-sm text-gray-400">
+          <p className="mb-2">&copy; 2024 STAR. All rights reserved.</p>
+          <p>
+            <a
+              href="https://www.hotstar.com/tnc/in"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-gray-400 mr-2"
+            >
+              <span>Terms of Use</span>
+            </a>
+            <a
+              href="https://www.hotstar.com/privacy-policy/in"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mr-2 text-gray-400 hover:text-gray-400"
+            >
+              <span>Privacy Policy</span>
+            </a>{" "}
+            <a
+              href="https://help.hotstar.com/in/en/support/home"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-gray-400"
+            >
+              <span>FAQ</span>
+            </a>
+          </p>
+        </div>
+        <div className="FooterImages flex flex-row">
+          <a
+            href="https://play.google.com/store/apps/details?id=in.startv.hotstar"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-400 hover:text-gray-400"
+          >
+            <img
+              src={GooglePlayImg}
+              alt="Google Play Store"
+              className="w-[120px] h-[40px]"
+            />
+          </a>
+          <a
+            href="https://apps.apple.com/in/app/hotstar-movies-live-cricket/id934459219"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-400 hover:text-gray-400"
+          >
+            <img
+              src={AppStoreImg}
+              alt="Appstore"
+              className="w-[120px] h-[40px]"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
