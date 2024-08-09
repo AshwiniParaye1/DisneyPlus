f3b2bdcf938a1a1e94115039b018c34418a7ba56
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-09 13:08:41 +0530

    changed spacing

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -7,7 +7,7 @@ function GenreMovieList() {
       {GenresList.genre.map(
         (item, index) =>
           index <= 4 && (
-            <div key={item.id} className="p-8 px-8 md:px-16">
+            <div key={item.id} className="p-5 px-8 md:px-16">
               <h2 className="text-[20px] text-white font-bold">{item.name}</h2>
 
               <MovieList genreId={item.id} index_={index} />
